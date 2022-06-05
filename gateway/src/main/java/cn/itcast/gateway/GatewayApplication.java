4448978235076d0cce0ce5097138a3c95526cd16
‘王晓波 <xinjing2020@aliyun.com>
2022-06-05 20:56:22 +0800

    新增网关服务    。对用户请求做身份认证，权限效验    。将用户请求路由到微服务，并实现负载均衡    。对用户请求做限流

---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wangxiaobo
+ * @create: 2022-06-05 20:15
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
